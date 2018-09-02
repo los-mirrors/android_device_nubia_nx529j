38e12c4ff914f5bce2c0b811294a5151eedd29a2
Bruno Martins <bgcngm@gmail.com>
2018-09-03 00:40:59 +0200

    nx529j: Adapt for local LineageHW java overlays
    Change-Id: Icb2b9810217f4b4d442ab83a16f455bd2ed82071
    (cherry picked from commit fab8114a98431c0f630f39d36b3651f3b3ff0558)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,8 +168,7 @@ TARGET_PROVIDES_KEYMASTER := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Lineage Hardware
-BOARD_HARDWARE_CLASS += \
-    $(DEVICE_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 # OTA Assert
 TARGET_OTA_ASSERT_DEVICE := Nubia Z11 Mini,z11 mini,NX529J,nx529j
